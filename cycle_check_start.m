ca900fd9fc2c7304a6b7175dd09b761755f79e4c
speckhard <dts@stanford.edu>
2016-10-08 04:33:34 +0200

    This is an initial commit of three matlab files
    cycle_check.m serves to find if adding a new branch will create a cycle in our tree
    
    cycle_check_start.m serves to find the optimal node in a potenitial branch to begin looking for cycles.
    
    mutual_information_computation_10_7_16.m is the main script to create a graph of the estimated grid topology.

---
+++ cycle_check_start.m
@@ -0,0 +1,23 @@
+function y = cycle_check_start(x,z,valid_pairs)
+
+% function to find the best starting point for checking if there
+% will be a cycle if the connection between x,z is retained
+
+list_of_connections_for_x = []; list_of_connections_for_z = [];
+
+for i = numel(valid_pairs(:,1))
+    if ismember(x, valid_pairs(i,:))
+        % if x is a member of the row, append row number
+        % to the list of connections
+        list_of_connections_for_x = [list_of_connections_for_x, i];
+    end
+    if ismember(z, valid_pairs(i,:))
+        list_of_connections_for_z = [list_of_connections_for_z, i];
+    end
+end
+
+if list_of_connections_for_x > list_of_connections_for_z
+    y = 1;
+else
+    y =  0;
+end
