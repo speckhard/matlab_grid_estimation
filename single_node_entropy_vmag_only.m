3594c0f0b25e1dc22e4e9530e12a00c2e474582f
speckhard <dts@stanford.edu>
2016-10-14 20:27:27 +0300

    single node entropy fxn using vmag only

---
+++ single_node_entropy_vmag_only.m
@@ -0,0 +1,18 @@
+% his function computes the entropy vector, h(i), of single nodes
+function single_node_entropy_vec = single_node_entropy(v_matrix)
+
+% this function takes in a 2d matrix of dimenstional array (observation
+% number, bus number ). 
+
+% calculate the number of buses (this is used in the for loop below)
+number_of_buses = numel(v_matrix(1,:));
+
+% initialize the entropy vector for which values will be calculated
+single_node_entropy_vec = zeros(1,number_of_buses); % intialize the vector
+
+for i=1:number_of_buses
+    % note here we only have on dimension, vmag, in our entropy calc
+    single_node_entropy_vec(i) = 1/2 * ...
+        (1+log(2*pi)) + 0.5*log(det(cov( ... 
+        v_matrix(:,i))));
+end
