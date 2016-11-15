ea806817d7f3dff8ed2222d631ccf014a93e422d
speckhard <dts@stanford.edu>
2016-11-15 10:20:55 -0800

    script will now compare MI method vs Noise wrt to SDR

--- grid_estimator_main_noise_11_08.m
+++ grid_estimator_main_noise_11_08.m
@@ -21,34 +21,38 @@ collapse_data = @collapse_redundant_data;
 remove_useless_branches = @remove_redundant_branches;
 true_branch_data = remove_useless_branches(true_branch_data); 
 %% Add Noise to Data
-percent_noise_vec = 1/100*[10, 1, 0.1, 0.01, 0.001, 0.0001];
-sdr_vec = zeros(numel(percent_noise_vec),1);
+percent_noise_vec = 1/100*[1E-4, 0.5E-3, 1E-3, 0.5E-2, 0.01E-2];
+sdr_vec = zeros(numel(percent_noise_vec),3);
 for i = 1:numel(percent_noise_vec);
     
     add_uniform_random_noise = @add_noise;
     node_volt_matrix_noise = add_uniform_random_noise(node_volt_matrix,...
         'local_mean', percent_noise_vec(i));
-
-    %Consider The Derivative Instead
+    %% Consider The Derivative Instead
     take_derivative = @consider_derivative;
     node_volt_matrix_noise_deriv = take_derivative(node_volt_matrix_noise);
-
-%% Downsample  Data to 60 mins
-% downsampler = @downsample_v;
-% downsampling_interval_mins = 60; % every sixty mins one sample is saved
-% node_volt_matrix_downsampled = downsampler(node_volt_matrix, ...
-%     downsampling_interval_mins, 'median', 'deriv');
-% mean(mean(node_volt_matrix_downsampled))
-% %% Digitize the Input
-% digitizer = @digitize_sig; % Import the digitizer
-% bits_desired = 8;
-% node_volt_matrix_digital = digitizer(node_volt_matrix_downsampled, ...
-%     bits_desired, 'local','local');
+    %% Digitize the Input
+    digitizer = @digitize_sig; % Import the digitizer
+    bits_desired = 8;
+    node_volt_matrix_digital = digitizer(node_volt_matrix_noise_deriv, ...
+        bits_desired, 'local','local');
+    
+    for j =1:3
+        if j ==1 
+            MI_method = 'gaussian';
+        elseif j ==2
+            MI_method = 'JVHW';
+        else MI_method = 'discrete';
+        end
+    
 %% Run Chow-Liu 
     compute_sdr = @run_chow_liu_options;
-    sdr_vec(i) = compute_sdr(node_volt_matrix_noise_deriv,...
+    sdr_vec(i,j) = compute_sdr(node_volt_matrix_digital,...
         true_branch_data, ...
-        'gaussian','no deriv', 'nograph','no heat_map','no discretization')
+        MI_method,'no deriv', 'nograph','no heat_map','no discretization')
+        
+    end
+    
 end
 
 
