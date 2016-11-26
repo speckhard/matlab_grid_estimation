6c0280e0cf7821989a1c3a1287c2b47dd376d523
speckhard <dts@stanford.edu>
2016-11-26 08:36:12 -0800

    adapted for all MI methods and now saves to a mat file

--- vary_deriv_main_11_26.m
+++ vary_deriv_main_11_26.m
@@ -14,14 +14,28 @@ true_branch_data = remove_useless_branches(true_branch_data);
 %% Consider The Variable Derivative, with a Gaussian MI
 vary_deriv_step = @var_deriv;
 delay_size_vec = [1, 2, 5, 10, 20, 30, 40, 50, 60]
-sdr_vec = zeros(1, numel(delay_size_vec));
+sdr_mat = zeros(3, numel(delay_size_vec));
+
 for i = 1:numel(delay_size_vec)
     node_volt_matrix_deriv = vary_deriv_step(node_volt_matrix,...
         delay_size_vec(i));
-    
-    compute_sdr = @run_chow_liu_options;
-    sdr = compute_sdr(node_volt_matrix_deriv,true_branch_data, ...
-        'gaussian','no_deriv','nograph','no heat_map',...
-        'no discretization')
-    sdr_vec(i) = sdr;
+    for j = 1:3
+        if j == 1
+            num_bits = 'no discretization';
+            MI_method = 'gaussian';
+        elseif j ==2
+            num_bits = 14;
+            MI_method = 'JVHW';
+        elseif j == 3
+            num_bits = 14;
+            MI_method = 'discrete';
+        end
+        compute_sdr = @run_chow_liu_options;
+        sdr = compute_sdr(node_volt_matrix_deriv,true_branch_data, ...
+            MI_method,'no_deriv','nograph','no heat_map',...
+            num_bits)
+        sdr_mat(j,i) = sdr;
+    end
 end
+
+save('sdr_varying_deriv_step_main_11_26','sdr_mat')
