1716150b79199654eeed0069de2ca0630cb9214d
speckhard <dts@stanford.edu>
2016-12-04 09:58:27 -0800

    syntax corrections

--- MI_discrete_tests.m
+++ MI_discrete_tests.m
@@ -15,34 +15,29 @@ collapse_data = @collapse_redundant_data;
 remove_useless_branches = @remove_redundant_branches;
 true_branch_data = remove_useless_branches(true_branch_data); 
 %% Consider The Derivative Instead
-
-time_resolution_vec = [5 15 30 60]; 
-sdr_mat = zeros(3,numel(time_resolution_vec));
-% Cycle through the time resolutions of data
-for j = 1:numel(time_resolution_vec);
-    % Downsample and take the derivative of the data. 
-    node_volt_matrix_downsampled_deriv = downsample_v(node_volt_matrix, ...
-    time_resolution_vec(j), 'last', 'deriv');
-
+take_derivative = @consider_derivative;
+node_volt_matrix = take_derivative(node_volt_matrix);
+%% Run Chow-Liu
+num_bits_vector = [12 14] %,6,8,10,12];
+sdr_mat = zeros(3, numel(num_bits_vector));
+for j = 1:numel(num_bits_vector);
     for i = 1:3
+        num_bits = num_bits_vector(j);
         if i == 1
             MI_vector = 'gaussian';
-            num_bits = 'no discretization';
+            %num_bits = 'no discretization';
         elseif i == 2
             MI_vector = 'JVHW';
-            num_bits = 14;
         else i == 3
             MI_vector = 'discrete';
-            num_bits = 14;
         end
         
         %% Run Chow-Liu 
         compute_sdr = @run_chow_liu_options;
-        sdr = compute_sdr(node_volt_matrix_downsampled_deriv,...
-            true_branch_data,MI_vector,'no_deriv','nograph',...
-            'no heat_map', num_bits)
+        sdr = compute_sdr(node_volt_matrix,true_branch_data, ...
+            MI_vector,'no_deriv','nograph','no heat_map', num_bits)
         sdr_mat(i,j) = sdr;
     end
 end
 %% Save Data
-save('SG_last_resolution_vs_SDR_12_3','sdr_mat')
+save('SG2_1min_12_2','sdr_mat')
