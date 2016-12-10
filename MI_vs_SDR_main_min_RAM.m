2c8145017d0e4ad3926974b4877eba7a055b2092
speckhard <dts@stanford.edu>
2016-12-09 17:36:44 -0800

    changed input file name.

--- MI_vs_SDR_main_min_RAM.m
+++ MI_vs_SDR_main_min_RAM.m
@@ -10,7 +10,7 @@
 % Column W corresponds to Node 1, Column KI corresponds to node 272. We
 % purposely leave out the feeder node since it's vmag value is not constant. 
 data_limits = 'W10..KI525610';
-node_volt_matrix = csvread('/afs/ir.stanford.edu/users/d/t/dts/Documents/Sandia Data/SG2_data_volt_1min.csv',...
+node_volt_matrix = csvread('/afs/ir.stanford.edu/users/d/t/dts/Documents/Rajagopal/Sandia Data/SG2_data_volt_1min.csv',...
    9,22, data_limits);
 % node_volt_matrix = v_vec(:,2:end);
 %% Second, copy the list of true branches.
