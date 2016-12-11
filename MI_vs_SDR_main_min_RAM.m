a11a2cf768edc0cb92b6bf9d63105705b0f30f0a
speckhard <dts@stanford.edu>
2016-12-11 09:59:36 -0800

    changed data lims to 1min dataset.

--- MI_vs_SDR_main_min_RAM.m
+++ MI_vs_SDR_main_min_RAM.m
@@ -7,10 +7,10 @@
 %% First, copy all except feeder SG data into a new matrix.
 % 60 min file has 8760 datapoints. End point 8770. 
 % 15 min file has 35040 datapoints. End point 35050
-% 1min file has 525600 datapoints. End point 515610
+% 1min file has 525600 datapoints. End point 525610
 % Column W corresponds to Node 1, Column KI corresponds to node 272. We
 % purposely leave out the feeder node since it's vmag value is not constant. 
-data_limits = 'W10..KI515610';
+data_limits = 'W10..KI525610';
 node_volt_matrix = csvread('/afs/ir.stanford.edu/users/d/t/dts/Documents/Rajagopal/Sandia Data/SG2_data_volt_1min.csv',...
    9,22, data_limits);
 %  node_volt_matrix = v_vec(:,2:end);
