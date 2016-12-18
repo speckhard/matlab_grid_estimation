7cdc0ad6eeefad1d49a582e87b88ece58c8fc28c
speckhard <dts@stanford.edu>
2016-12-17 20:14:14 -0500

    parallel barley script.

---
+++ barley_par.sh
@@ -0,0 +1,13 @@
+
+
+#!/bin/bash
+
+#$ -N matlab_par_JVHW
+#$ -cwd
+#$ -M dts@stanford.edu
+#$ -m besa
+#$ -j yes
+#$ -pe shm 8
+
+module load matlab
+matlab -nodesktop -r 'maxNumCompThreads' </afs/ir.stanford.edu/users/d/t/dts/Documents/Rajagopal/Matlab_Code/Rajagopal_Grid_Estimation/parallel_JVHW_test.m
