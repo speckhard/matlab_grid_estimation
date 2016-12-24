3ce1e7f1f33c223f6bac766b3756048d9a112d7c
speckhard <dts@stanford.edu>
2016-12-24 03:08:03 -0500

    name change.

--- barley_par.sh
+++ barley_par.sh
@@ -2,12 +2,13 @@
 
 #!/bin/bash
 
-#$ -N matlab_par_sig_dig_SG_v1
+#$ -N matlab_par_lens_vs_res_SG2_solar_v1
 #$ -cwd
 #$ -M dts@stanford.edu
 #$ -m besa
 #$ -j yes
 #$ -pe shm 8
+#$ -mem_free=9G
 
 module load matlab
-matlab -nodesktop -r 'maxNumCompThreads' </afs/ir.stanford.edu/users/d/t/dts/Documents/Rajagopal/Matlab_Code/Rajagopal_Grid_Estimation/sig_dig_main.m
+matlab -nodesktop -r 'maxNumCompThreads' </afs/ir.stanford.edu/users/d/t/dts/Documents/Rajagopal/Matlab_Code/Rajagopal_Grid_Estimation/lens_vs_res_vs_SDR.m
