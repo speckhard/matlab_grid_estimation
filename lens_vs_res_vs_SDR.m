abd15b896ed675dd28ed77a331de6dfe8b58b354
speckhard <dts@stanford.edu>
2016-12-24 03:07:49 -0500

    parallel environment.

--- lens_vs_res_vs_SDR.m
+++ lens_vs_res_vs_SDR.m
@@ -1,5 +1,16 @@
 % Scirpt to find the x node SDR and regular SDR for different lenses of
 % data and different first type downsampling resoltions.
+%% Initialize Parallel Cluster Environment
+cluster = parcluster('local')
+tmpdirforpool = tempname
+mkdir(tmpdirforpool)
+cluster.JobStorageLocation = tmpdirforpool
+
+msg = sprintf('setting matlabpool to %s', getenv('NSLOTS'))
+cluster.NumWorkers = str2num(getenv('NSLOTS'))
+
+parpool(cluster)
+isempty(gcp('nocreate'))
 
 %% Import Data to analyze 
 % First, copy the data minus the feeder bus
@@ -147,5 +158,7 @@ results = struct(field1, value1, field2, value2, field3, value3,...
     field4, value4, field5, value5, field6, value6, field7, value7, ...
     field8, value8, field9, value9, field10, value10);
 % Save a .mat file.
-save('/afs/ir.stanford.edu/users/d/t/dts/Documents/Rajagopal/Results/SG2_solar_deriv_lens_res_corn_12_23_v1'...
+save('/afs/ir.stanford.edu/users/d/t/dts/Documents/Rajagopal/Results/SG2_solar_deriv_lens_res_barley_12_23_v1'...
      ,'results')
+ %% Close Matlab Parallel Environment
+delete(gcp('nocreate'))
