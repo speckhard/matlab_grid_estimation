c4da013b2d89c4803b979338199c75baf90d6824
speckhard <dts@stanford.edu>
2016-12-24 02:46:27 -0500

    running sig dig

--- barley_par.sh
+++ barley_par.sh
@@ -2,7 +2,7 @@
 
 #!/bin/bash
 
-#$ -N matlab_par_JVHW_lin_parfor_v9
+#$ -N matlab_par_sig_dig_SG_v1
 #$ -cwd
 #$ -M dts@stanford.edu
 #$ -m besa
@@ -10,4 +10,4 @@
 #$ -pe shm 8
 
 module load matlab
-matlab -nodesktop -r 'maxNumCompThreads' </afs/ir.stanford.edu/users/d/t/dts/Documents/Rajagopal/Matlab_Code/Rajagopal_Grid_Estimation/parallel_JVHW_test.m
+matlab -nodesktop -r 'maxNumCompThreads' </afs/ir.stanford.edu/users/d/t/dts/Documents/Rajagopal/Matlab_Code/Rajagopal_Grid_Estimation/sig_dig_main.m
