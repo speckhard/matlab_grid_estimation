3a3a6ad64688723fd33ad7e44ac8bf6a9b0bfea8
speckhard <dts@stanford.edu>
2016-12-25 01:12:00 -0500

    fixed MI saving error

--- barley_par.sh
+++ barley_par.sh
@@ -11,4 +11,4 @@
 #$ -l mem_free=4.2G
 
 module load matlab
-matlab -nodesktop -r 'maxNumCompThreads' </afs/ir.stanford.edu/users/d/t/dts/Documents/Rajagopal/Matlab_Code/Rajagopal_Grid_Estimation/dsample_method_main.m
+matlab -nodesktop -r 'maxNumCompThreads' </afs/ir.stanford.edu/users/d/t/dts/Documents/Rajagopal/Matlab_Code/Rajagopal_Grid_Estimation/num_bits_main.m
