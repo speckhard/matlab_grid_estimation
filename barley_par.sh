85edeeeedbbf89b9c578e0107dae76656f15e616
speckhard <dts@stanford.edu>
2016-12-31 14:20:39 -0500

    SG2 solar

--- barley_par.sh
+++ barley_par.sh
@@ -3,6 +3,7 @@
 #!/bin/bash
 
 #$ -N matlab_par_noise_SG1solar_v1
+#$ - 
 #$ -M dts@stanford.edu
 #$ -m besa
 #$ -j yes
@@ -10,4 +11,4 @@
 #$ -l mem_free=4.2G
 
 module load matlab
-matlab -nodesktop -r 'maxNumCompThreads' </afs/ir.stanford.edu/users/d/t/dts/Documents/Rajagopal/Matlab_Code/Rajagopal_Grid_Estimation/noise_vs_SDR.m
+matlab -nodesktop -r 'maxNumCompThreads' </afs/ir.stanford.edu/users/d/t/dts/Documents/Rajagopal/Matlab_Code/Rajagopal_Grid_Estimation/vary_deriv_main.m
