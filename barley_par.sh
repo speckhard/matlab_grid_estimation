1fc3c9c455fbd2d8a0501feb924cff1351f28531
speckhard <dts@stanford.edu>
2017-02-11 09:14:15 -0800

    4.2g ram

--- barley_par.sh
+++ barley_par.sh
@@ -8,7 +8,7 @@
 #$ -m besa
 #$ -j yes
 #$ -pe shm 8
-#$ -l mem_free=9.2G
+#$ -l mem_free=4.2G
 
 module load matlab
 matlab -nodesktop -r 'maxNumCompThreads' </afs/ir.stanford.edu/users/d/t/dts/Documents/Rajagopal/Matlab_Code/Rajagopal_Grid_Estimation/num_bits_main.m
