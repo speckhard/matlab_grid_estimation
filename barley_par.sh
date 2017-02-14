f5d5b1eade589faffa822a508efdb2fa1c0b1af2
speckhard <dts@stanford.edu>
2017-02-13 16:59:00 -0800

    barley

--- barley_par.sh
+++ barley_par.sh
@@ -2,13 +2,13 @@
 
 #!/bin/bash
 
-#$ -N matlab_par_num_bits_lens_MI_D_only_SG2_021017_v1
+#$ -N matlab_par_num_bits_lens_MI_G_only_SG2_021017_v3
 #$ -cwd
 #$ -M dts@stanford.edu
 #$ -m besa
 #$ -j yes
 #$ -pe shm 8
-#$ -l mem_free=4.2G
+#$ -l mem_free=5.2G
 
 module load matlab
 matlab -nodesktop -r 'maxNumCompThreads' </afs/ir.stanford.edu/users/d/t/dts/Documents/Rajagopal/Matlab_Code/Rajagopal_Grid_Estimation/num_bits_main.m
