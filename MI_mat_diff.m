08c35256e31623bad2a71168ce19a2601aa0e08c
speckhard <dts@stanford.edu>
2017-04-06 06:08:08 -0700

    rough draft to find abs diff between MI mats

---
+++ MI_mat_diff.m
@@ -0,0 +1,19 @@
+function [max, min, mean, stdev] = MI_mat_diff(MI_mat_x, MI_mat_y);
+
+% Find the error between two MI matrices
+
+% Check to see if both matrices are the same size
+if size(MI_mat_x) ~= size(MI_mat_y)
+    error('input matrices are not the same size')
+end
+
+num_buses = numel(MI_mat_x(:,1));
+abs_diff_vec = zeros(1,num_buses*(num_buses-1));
+
+for i = 2:num_buses
+    for j = 1: i -1
+        abs_diff_vec(i+j - 3) = abs(MI_mat_x(i,j) - MI_mat_y(i,j))
+    end
+end
+
+        
