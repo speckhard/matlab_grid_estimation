a8ce3ef69c9db72c5f9b6c6704db0ae449a3aed7
speckhard <dts@stanford.edu>
2017-04-06 06:15:32 -0700

    cleaned up comments

--- var_deriv.m
+++ var_deriv.m
@@ -1,18 +1,34 @@
-function node_volt_deriv_mat = var_deriv(node_volt_mat, deriv_step)
+function node_volt_matrix = var_deriv(node_volt_matrix, deriv_step)
 
-% This function takes a varying difference of the node volt data depending
-% on the vaue of deriv step.
-
-% Input: 
-% node_volt_mat: voltage measurements at different nodes (columns signify
-% different nodes) and at different times (rows). Time step assumed to be
-% the same unit as deriv step. If not conversion must be performed prior to
-% input. 
-% deriv_step: The voltage(time t, node x)' = v(t, x) - v(t-deriv_step,x).
-
-% Output: Is the voltage subtracted by the a variable step in the past at
-% the same node as defined above in deriv step.
+% This function finds the change in voltage magntiude for a variable
+% difference of time-points. The change in voltage magnitude is calculated 
+% as Delta_Vmag = Vmag(node x, time t) - mag(node x, time t-deriv_step).
+%
+% node_volt_deriv_mat = var_deriv(node_volt_matrix, deriv_step)
+%
+% Input:
+% ----- node_volt_matrix: A matrix containing the voltage magnitude data.
+% The number of columns should be the number of nodes in the system. The
+% number of rows in the matrix corresponds to the number of datapoints
+% where the voltage magnitude is measured across all nodes.
+% ----- deriv_step: This is an integer specifiying the number of timepoints
+% for which the change in voltage magntiude is calculated. The change in 
+% voltage magnitude is calculated as Delta_Vmag = Vmag(node x, time t) - 
+% Vmag(node x, time t-deriv_step).
+% Output:
+% ----- node_volt_matrix: The output matrix is the same size as the input
+% except that it has one less row of data. The first deriv_step (this is an
+% integer) rows of data are removed after the change in voltage magnitude
+% is calculated since the first deriv_step rows have no previous time point 
+% with which we find the change in voltage magnitude. The values in the 
+% output matrix are equal to the  change in voltage mangnitude, Delta_Vmag 
+% = Vmag(node x, time t) - Vmag(node x, time t-deriv_step). We use the same 
+% name for the output as for the input since this matrix can be quite large
+% and creating a newly named variable results in an extra copy of the data 
+% created by Matlab.
 
 delay = deriv_step;
-node_volt_deriv_mat = node_volt_mat(delay+1:end,:)-...
-    node_volt_mat(1:end-delay,:);
+node_volt_matrix = node_volt_matrix(delay+1:end,:)-...
+    node_volt_matrix(1:end-delay,:);
+end
+
