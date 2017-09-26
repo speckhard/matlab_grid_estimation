9bc10bc91a7259f7ea22ba8f04789e15337b7c63
speckhard <dts@stanford.edu>
2017-09-25 21:36:57 -0700

    KL_divergence.m

--- KL_divergence.m
+++ KL_divergence.m
@@ -1,26 +1,33 @@
 % Script to find DL between gaussian and pdf.
 
 %%% 
-% data_limits = 'W10..BV8770'
-% 
-% node_volt_matrix2 = csvread('/Users/Dboy/Downloads/SG_data_solar_60min.csv',...
-%    9,22, data_limits);
-%% True branches 
-data_limits = 'A1..B271';%'A1..B51';%'A1..B271';
-true_branch_data = csvread('/Users/Dboy/Documents/Stanny/Rajagopal/github_scripts/SG2_true_branch_list.csv',...
-    0,0, data_limits);
-%% Vmag data 
-data_limits = 'W10..KH525610'
+data_limits = 'W10..BV8770'
 
-node_volt_matrix2 = csvread('/Users/Dboy/Downloads/SG2_data_solar_1min.csv',...
+node_volt_matrix2 = csvread('/Users/Dboy/Downloads/SG_data_solar_60min.csv',...
    9,22, data_limits);
+% %% True branches 
+% data_limits = 'A1..B271';%'A1..B51';%'A1..B271';
+% true_branch_data = csvread('/Users/Dboy/Documents/Stanny/Rajagopal/github_scripts/SG2_true_branch_list.csv',...
+%     0,0, data_limits);
+% %% Vmag data 
+% data_limits = 'W10..KH525610'
+% % 
+% node_volt_matrix2 = csvread('/Users/Dboy/Downloads/SG2_data_solar_1min.csv',...
+%     9,22, data_limits);
+
+% load('/Users/Dboy/Downloads/Node123_randPF.mat')
+% true_branch_data = squeeze(mpc_base.branch(2:end,1:2));
+% node_volt_matrix2 = v_vec(:,2:end);
 %% Remove redundancies
 collapse_data = @collapse_redundant_data;
 [node_volt_matrix2, true_branch_data] = ...
     collapse_data(node_volt_matrix2, true_branch_data);
 %% Remove Redundant Branches
-remove_useless_branches = @remove_redundant_branches;
-true_branch_data = remove_useless_branches(true_branch_data);
+% remove_useless_branches = @remove_redundant_branches;
+% true_branch_data = remove_useless_branches(true_branch_data);
+
+% load('/Users/Dboy/Documents/Stanny/Rajagopal/LV_data/MV_urban.mat')
+% node_volt_matrix2 = v_vec(:,1:end-2);
 %%
 take_deriv = @consider_derivative;
 node_volt_matrix2 = take_deriv(node_volt_matrix2);
@@ -43,9 +50,9 @@ end
 %mean(node_volt_matrix2(:,node));
 % Plot the histogram data
 % hold off
-% plot(x_vals, N)
+plot(x_vals, N)
 
-% Plot the gaussian fit, extract the mean, sigma
+%% Plot the gaussian fit, extract the mean, sigma
 hold on
 mu = mean(node_volt_matrix2(:,node));
 sigma = std(node_volt_matrix2(:,node));
@@ -53,13 +60,18 @@ sigma = std(node_volt_matrix2(:,node));
 %     max(node_volt_matrix(:,b));
 y = x_vals(:,1);
 f = exp(-(y-mu).^2./(2*sigma^2))./(sigma*sqrt(2*pi));
-% plot(y,f,'LineWidth',2.5)
-
+plot(y,f,'LineWidth',2.5)
+%%
+xlabel('Change in Voltage Magnitude (V)')
+ylabel('Probability Density')
+set(gca,'FontSize',18);
+legend('histogram', 'Gaussian approx.')
+%%
 % Calculate the DL divergence
 KL_sum = 0;
 for i = 1:numel(y)
-    if N(i) ~= 0
-        KL_sum = N(i)*log(N(i)/f(i)) + KL_sum;
+    if (N(i) > 0 ) && (f(i) > 1E-3) 
+        KL_sum = N(i)*log2(N(i)/f(i)) + KL_sum;
     end
 end
 
@@ -67,5 +79,9 @@ KL_sum_vec(j) = KL_sum;
 end
 
 %% Find the node with the most divergence
-
+KL_stats = zeros(1,4);
+KL_stats(1) = max(KL_sum_vec);
+KL_stats(2) = min(KL_sum_vec);
+KL_stats(3) = mean(KL_sum_vec);
+KL_stats(4) = std(KL_sum_vec);
 
---
+++ run_chow_liu_return_list.m
@@ -0,0 +1,116 @@
+function [sdr_percent, estimated_node_pairs] = run_chow_liu_return_list(Node_Volt_Matrix,...
+    true_branch_pairs, entropy_flag, deriv_flag, graph_flag, ...
+    MI_heatmap_flag, num_bits)
+
+% This function takes in a matrix of voltage measurments, a list of true
+% branches and flag that determines if the data will be estimated as
+% gaussian or with the JVHW estimator.
+
+if strcmp(num_bits,'no discretization') ~= 1
+    if isinteger(int8(num_bits)) ~= 1
+        error('Num Bits (the last arg) is not an integer below 255')
+    end
+    disp('digitizing input to') 
+    disp(num_bits)
+    %% Digitize the Input
+    digitizer = @digitize_sig; % Import the digitizer
+    Node_Volt_Matrix = digitizer(Node_Volt_Matrix, ...
+    num_bits, 'local','local');
+end
+
+if strcmp(deriv_flag, 'deriv')
+    %% Take the derivative of the data
+    disp('taking deriv of data')
+    take_derivative = @consider_derivative;
+    Node_Volt_Matrix = take_derivative(Node_Volt_Matrix);
+end
+% If the entropy_flag is the string gaussian in lower case, proceed to
+% model the data as guassian to find the MI.
+if strcmp(entropy_flag, 'gaussian')
+    
+    %% Find the entropy of each node, H(i).
+    tic
+    find_single_node_entropy = @single_node_entropy_vmag_only;
+    single_node_entropy_vec = find_single_node_entropy(Node_Volt_Matrix);
+    disp('time required to find entropy')
+    toc
+    %% Find the joint entropy of pairs of different nodes, H(i,j).
+    tic
+    find_joint_entropy = @joint_entropy_vmag_only_fixed_neglog;
+    joint_entropy_matrix = find_joint_entropy(Node_Volt_Matrix);
+    disp('time required to find joint entropy')
+    toc
+    %% Find the mutual information of pairs of different nodes, I(i,j).
+    tic
+    find_mutual_information = @mutual_information;
+    mutual_information_matrix = find_mutual_information(...
+        single_node_entropy_vec, joint_entropy_matrix);
+    disp('time required to find mutual information')
+    toc
+    
+elseif strcmp(entropy_flag, 'JVHW')
+    %% Find the JVHW MI
+    tic
+    JVHW_MI = @find_JVHW_MI;
+    mutual_information_matrix = JVHW_MI(Node_Volt_Matrix, ...
+        'no diagonals'); % This last option, ensures diagonal values
+    % are not computed.
+    disp('time to find the JVHW MI')
+    toc 
+elseif strcmp(entropy_flag, 'discrete')
+    tic
+    find_discrete_MI = @MI_vmag_discrete;
+    mutual_information_matrix = find_discrete_MI(Node_Volt_Matrix, ...
+        num_bits);
+    disp('Time to find the discrete MI')
+    toc
+else
+    error('entropy flag can only be gaussian or JVHW')
+end
+
+%% Find Min Span Tree using Kruskal.m
+tic
+find_min_span_tree = @kruskal;
+mat_of_connectivity = find_min_span_tree(mutual_information_matrix);
+disp('time required to run Kruskal')
+toc
+
+%% Find the pairs from the Matrix of Connectivity
+number_of_buses = numel(Node_Volt_Matrix(1,:));
+estimated_node_pairs = zeros(number_of_buses - 1,2);
+p = 0;
+for i = 2:number_of_buses
+    for k = 1:i-1
+        if (mat_of_connectivity(i,k) ~=0)
+            p = p +1;
+            estimated_node_pairs(p,:) = [i,k];
+        end
+    end
+end
+
+
+%% Find the SDR
+find_sdr = @findSDR;
+[succesful_branch_counter, SDR] = ...
+    find_sdr(estimated_node_pairs, true_branch_pairs);
+
+sdr_percent = SDR;
+
+%% Graph the Tree if graph_flag is set
+if strcmp(graph_flag, 'graph')
+    
+    G = graph(estimated_node_pairs(:,1),estimated_node_pairs(:,2));
+    
+    % the default nodelables are too small to read in the 123 node grid
+    % we erase them and add them again with font size 7.
+    % This is a workaround for the lack of nodelabel.fontsize obj property
+    % in node graphs in matlab.
+    h = plot(G, 'NodeLabel', []);
+    
+    for i=1:length(h.XData)
+        text(h.XData(i),h.YData(i),num2str(i),'fontsize',18);
+    end
+end
+end
+
+    
